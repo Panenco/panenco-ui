d64cde061af66812ae8085817366b8971ee49c5d
Oleksii <kot.aleksei43@gmail.com>
2020-09-22 08:16:13 +0300

    fix: separate select to logic parts

---
+++ src/components/select/select-component.tsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import Select, { components, Props as SelectProps } from 'react-select';
+import { InputComponent } from 'utils/types';
+import { useTheme, useMode } from 'utils/hooks';
+import CreatableSelect from 'react-select/creatable';
+import AsyncSelect from 'react-select/async';
+import AsyncCreatableSelect from 'react-select/async-creatable';
+import { Icon, Text } from 'components';
+import { customStyles } from './style';
+
+const CustomOption = (props: any): JSX.Element => {
+  const {
+    children,
+    selectProps: { formatCreateLabel: notFoundLabel, inputValue },
+    options,
+    isSelected,
+    label,
+  } = props;
+
+  let showButton = false; // btn add new option
+
+  if (options && notFoundLabel && label === notFoundLabel()) {
+    showButton = true;
+  }
+
+  return (
+    <components.Option {...props}>
+      {isSelected && <Icon icon={Icon.icons.check} />}
+      {children}
+      {showButton && <Text className="addNewOption">Create "{inputValue}"</Text>} {/* eslint-disable-line*/}
+    </components.Option>
+  );
+};
+
+const MultiValue = () => null;
+
+export interface ComponentProps extends SelectProps, InputComponent {
+  async?: boolean;
+  creatable?: boolean;
+  value?: any;
+  selectWrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+  clearChips?: boolean;
+  wrapperSelectSizes?: {
+    l?: number | string;
+    m?: number | string;
+    s?: number | string;
+  };
+}
+
+const Component = ({
+  error,
+  options,
+  title,
+  subTitle,
+  async,
+  creatable,
+  components: propComponents,
+  styles,
+  loadingMessage,
+  noOptionsMessage,
+  filterOption,
+  ...props
+}: ComponentProps): JSX.Element => {
+  let SelectComponent: any = Select;
+  const theme = useTheme();
+  const { mode } = useMode();
+
+  if (async) {
+    SelectComponent = AsyncSelect;
+  } else if (creatable) {
+    SelectComponent = CreatableSelect;
+  }
+  if (async && creatable) {
+    SelectComponent = AsyncCreatableSelect;
+  }
+
+  const customFilterOption = (option, input): boolean => {
+
+    if (input) {
+      return option?.label.toLowerCase().includes(input.toLowerCase());
+    }
+    return true;
+  };
+
+  return (
+    <>
+      {title && (
+        <Text className="title" weight={theme.typography.weights.bold}>
+          {title}
+        </Text>
+      )}
+      {subTitle && (
+        <Text className="subTitle" size={theme.typography.sizes.xs} color={theme.colors.secondary}>
+          {subTitle}
+        </Text>
+      )}
+      <div>
+        <div className="wrapperSelect">
+          <SelectComponent
+            options={options}
+            loadingMessage={
+              loadingMessage || (({ inputValue }): string => (inputValue ? `Loading ${inputValue}` : 'Loading...'))
+            }
+            styles={{ ...customStyles(theme, mode, error, styles) }}
+            components={{ Option: CustomOption, MultiValue, ...propComponents }}
+            noOptionsMessage={noOptionsMessage || (() => `Not found`)}
+            filterOption={filterOption || customFilterOption}
+            error={error}
+            isClearable={false}
+            {...props}
+          />
+          {error && (
+            <div className="errorIconWrapper">
+              <Icon className="errorIcon" icon={Icon.icons.close} />
+            </div>
+          )}
+        </div>
+        {error && (
+          <Text className="errorTitle" size={theme.typography.sizes.xs} color={theme.colors.error}>
+            {error}
+          </Text>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Component;
--- src/components/select/style.ts
+++ src/components/select/style.ts
@@ -16,6 +16,11 @@ export const customStyles = (theme: PUITheme, mode?: string, error?: any, styles
       ...provided,
       color: mode === ThemeMode.dark ? theme.colors.light : theme.colors.secondary,
       fontWeight: weights.regular,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      paddingRight: '5px',
+      width: '100%',
       ...additionalStyles('placeholder', styles, provided, state),
     };
   },
@@ -281,6 +286,7 @@ export const StyledSelectWrapper = styled.div`
   }
 
   .errorTitle {
+    position: absolute;
     color: ${(props: any): string =>
       props.mode === ThemeMode.dark ? props.theme.colors.light : props.theme.colors.error};
     display: block;
@@ -294,8 +300,8 @@ export const StyledSelectWrapper = styled.div`
 
     .errorIconWrapper {
       display: none;
-      /* opacity: 0; */
     }
+
     .title,
     .subTitle {
       margin-right: 0;
