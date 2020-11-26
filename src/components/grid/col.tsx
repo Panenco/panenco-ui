d53e094ed72df8bdd56e3837e5a3c05394fe23eb
Oleksii <kot.aleksei43@gmail.co>
2020-11-26 16:19:56 +0200

    refactor: change default bevaviour for grid layout (col element now expands to max width )

--- src/components/grid/col.tsx
+++ src/components/grid/col.tsx
@@ -8,7 +8,7 @@ interface ColProps extends React.HTMLAttributes<HTMLDivElement> {
   l?: number | string;
 }
 
-export const Col = ({ className, s = 1, m = 1, l = 1, children, ...props }: ColProps): JSX.Element => {
+export const Col = ({ className, s = 4, m = 8, l = 12, children, ...props }: ColProps): JSX.Element => {
   return (
     <StyledCol {...props} className={cx(`col-l-${l}`, `col-m-${m}`, `col-s-${s}`, className)}>
       {children}
