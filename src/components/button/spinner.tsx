afcdf4f8c69d64dcc8e5c2705fa68ae4d8cef97c
Vladyslav Chernii <vlad.chernij12@gmail.com>
2021-06-10 11:23:30 +0300

    fix: unused className

--- src/components/button/spinner.tsx
+++ src/components/button/spinner.tsx
@@ -1,10 +1,8 @@
-import cx from 'classnames';
 import { Icon } from 'components';
 import * as React from 'react';
 
-
 const Spinner = React.forwardRef<any, any>(() => {
-  return <Icon icon={Icon.icons.loader} className={cx('loader', 'spinner')} />;
+  return <Icon icon={Icon.icons.loader} className="spinner" />;
 });
 
 export default Spinner;
