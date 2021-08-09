2b4a8e127985dbb2eac21136622bb0852e84b6f4
Taras Venger <taras.venger@panenco.com>
2021-08-09 13:44:25 +0300

    feat: desktop tabs

---
+++ src/components/tabs/tab/index.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import cx from 'classnames';
+import { useTheme, useMode } from 'utils/hooks';
+import { Icon } from 'components';
+import { useCombinedRefs } from 'utils/hooks/combinedrefs';
+import { useTabContext, getTabPanelId } from '../tabContext';
+import { StyledTab } from './style';
+
+export interface TabProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  index: number;
+  className?: string;
+  iconClassName?: string;
+  icon: HTMLObjectElement;
+  to: string;
+  disabled?: boolean;
+  selected?: boolean;
+  children: React.ReactNode;
+}
+
+const BaseTab = React.forwardRef<any, TabProps>(
+  (
+    { index, className, iconClassName, icon, style, disabled, selected, onClick, children, ...props }: TabProps,
+    ref,
+  ): JSX.Element => {
+    const theme = useTheme();
+    const { mode } = useMode();
+    const tabRef = React.useRef();
+    const combinedRef = useCombinedRefs(tabRef, ref);
+
+    const context = useTabContext()!;
+    const { indexSelected, setIndexSelected } = context;
+
+    React.useEffect(() => {
+      if (selected) setIndexSelected(index);
+    }, []);
+
+    const isSelected = indexSelected === index;
+
+    const handleClick = (event): void => {
+      setIndexSelected(index);
+      if (onClick) onClick(event);
+    };
+
+    return (
+      <StyledTab
+        theme={theme}
+        mode={mode}
+        style={style}
+        role="tab"
+        disabled={disabled}
+        aria-selected={isSelected}
+        aria-controls={getTabPanelId(context, index)}
+        tabIndex={isSelected ? 0 : -1}
+        className={cx(className, disabled && 'disabled', isSelected && 'selected')}
+        onClick={handleClick}
+        ref={combinedRef}
+        {...props}
+      >
+        {icon && <Icon icon={icon} className={cx('iconClass', iconClassName)} />}
+        {children}
+      </StyledTab>
+    );
+  },
+);
+
+export const Tab = React.forwardRef(
+  ({ className, ...props }: TabProps, ref): JSX.Element => (
+    <BaseTab ref={ref} className={cx('tab', className)} {...props} />
+  ),
+);
+
+export const BookmarkTab = React.forwardRef(
+  ({ className, ...props }: TabProps, ref): JSX.Element => (
+    <BaseTab ref={ref} className={cx('bookmarkTab', className)} {...props} />
+  ),
+);
---
+++ stories/tabs/index.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Row, Col, Tab, BookmarkTab, TabContext, Tablist, Tabpanel, Icon } from 'components';
+import { BrowserRouter } from 'react-router-dom';
+
+import TabsDocs from 'components/tabs/tabs-DOCS.md';
+import TabsReadme from 'components/tabs/tabs-README.md';
+
+import { decorator } from '../../utils/decorator';
+import { WrappedComponent } from '../helpers/wrapped';
+
+const tablistStyle = { justifyContent: 'flex-start' };
+const tabPanelStyle = { padding: '22px', backgroundColor: '#FFFFFF', justifyContent: 'flex-start' };
+
+export default decorator('Tab', TabsDocs, TabsReadme).add('Tab component', () => {
+  return (
+    <WrappedComponent>
+      <BrowserRouter>
+        <Row>
+          <Col>
+            <TabContext>
+              <Tablist style={tablistStyle}>
+                <Tab index={0} icon={Icon.icons.settings}>
+                  Tab 1
+                </Tab>
+                <Tab index={1} icon={Icon.icons.settings}>
+                  Tab 2
+                </Tab>
+                <Tab index={2} icon={Icon.icons.settings}>
+                  Tab 3
+                </Tab>
+                <Tab index={3} icon={Icon.icons.settings}>
+                  Tab 4
+                </Tab>
+              </Tablist>
+              <Tabpanel index={0} style={tabPanelStyle}>
+                Tab panel 1
+              </Tabpanel>
+              <Tabpanel index={1} style={tabPanelStyle}>
+                Tab panel 2
+              </Tabpanel>
+              <Tabpanel index={2} style={tabPanelStyle}>
+                Tab panel 3
+              </Tabpanel>
+              <Tabpanel index={3} style={tabPanelStyle}>
+                Tab panel 4
+              </Tabpanel>
+            </TabContext>
+          </Col>
+        </Row>
+
+        <Row style={{ backgroundColor: '#ECEFF1', padding: '10px', marginTop: '20px' }}>
+          <Col>
+            <TabContext>
+              <Tablist autoActivation style={tablistStyle}>
+                <BookmarkTab index={0} icon={Icon.icons.settings}>
+                  Tab 1
+                </BookmarkTab>
+                <BookmarkTab index={1} icon={Icon.icons.settings}>
+                  Tab 2
+                </BookmarkTab>
+                <BookmarkTab index={2} icon={Icon.icons.settings} selected>
+                  Tab 3
+                </BookmarkTab>
+                <BookmarkTab index={3} icon={Icon.icons.settings} disabled>
+                  Tab 4
+                </BookmarkTab>
+              </Tablist>
+              <Tabpanel index={0} style={tabPanelStyle}>
+                Tab panel 1
+              </Tabpanel>
+              <Tabpanel index={1} style={tabPanelStyle}>
+                Tab panel 2
+              </Tabpanel>
+              <Tabpanel index={2} style={tabPanelStyle}>
+                Tab panel 3
+              </Tabpanel>
+              <Tabpanel index={3} style={tabPanelStyle}>
+                Tab panel 4
+              </Tabpanel>
+            </TabContext>
+          </Col>
+        </Row>
+      </BrowserRouter>
+    </WrappedComponent>
+  );
+});
