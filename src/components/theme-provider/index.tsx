2144843997332f1565a95bcf97f245f2688fe1ab
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2022-07-19 15:15:10 +0200

    fix: pre-alpha-styled-components inject-styles-first

--- src/components/theme-provider/index.tsx
+++ src/components/theme-provider/index.tsx
@@ -8,13 +8,29 @@ interface ThemeProviderProps {
   children: React.ReactNode;
   theme?: PUITheme;
   mode?: ThemeMode;
+  /**
+   * By default, the styles are injected last in the <head> element of the page.
+   * As a result, they gain more specificity than any other style sheet.
+   * If you don't want to override PUI's styles, set this prop to false.
+   */
+  injectStylesFirst?: boolean;
 }
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   children,
   theme: themeToUse = defaultTheme,
   mode: initialMode = ThemeMode.light,
+  injectStylesFirst = true,
 }: ThemeProviderProps) => {
+  if (injectStylesFirst && typeof window !== 'undefined') {
+    const { head } = document;
+    if (!head.querySelector('[data-styled="active"]')) {
+      const injectFirstNode = document.createElement('style');
+      injectFirstNode.setAttribute('data-styled', 'active');
+      head.insertBefore(injectFirstNode, head.firstChild);
+    }
+  }
+
   const [mode, setMode] = React.useState(initialMode);
 
   const theme = {
