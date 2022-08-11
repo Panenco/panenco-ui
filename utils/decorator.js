6623f6d821cb0bbaff010d42d2b13c8ace8e9ae4
vlacher12 <vlad.chernij12@gmail.com>
2022-08-11 16:11:21 +0300

    fix: removed webpack + upraded deps

--- utils/decorator.js
+++ utils/decorator.js
@@ -1,17 +1,16 @@
+// eslint-disable-next-line import/no-unresolved
 import * as React from 'react';
+// eslint-disable-next-line import/no-unresolved
 import { storiesOf } from '@storybook/react';
 // import { withKnobs } from '@storybook/addon-knobs';
-import { withReadme, withDocs } from 'storybook-readme';
 import { ThemeProvider } from 'components/theme-provider';
 import ScreenDimensions from './ScreenDimensions';
 // import defaultTheme from 'themes/default';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const decorator = (title, doc, readme) =>
+export const decorator = (title) =>
   storiesOf(title, module)
     // .addDecorator(withKnobs)
-    .addDecorator(withDocs(doc))
-    .addDecorator(withReadme(readme))
     .addDecorator((story) => (
       <ThemeProvider>{title === 'Grid' ? <ScreenDimensions>{story()}</ScreenDimensions> : story()}</ThemeProvider>
     ));
