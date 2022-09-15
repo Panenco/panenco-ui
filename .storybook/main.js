e1c7dc24a33fee617ffe9cfdd9e083b41982ccb0
Kyrylo Kurylchenko <kyryl.kurylchenko@gmail.com>
2022-09-15 14:46:09 +0300

    feat: add accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,13 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   "stories": [
-    "../stories/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   webpackFinal: async (config, { configType }) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()];
