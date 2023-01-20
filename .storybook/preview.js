ee44dc0057c4f64d067aeb46e297f2bafcf80f95
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2023-01-20 13:29:29 +0100

    feat: open docs by default

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,12 @@ import '!style-loader!css-loader!./styles.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewMode: 'docs',
+  previewTabs: { 
+    'storybook/docs/panel': {
+      index: -1,
+    },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
