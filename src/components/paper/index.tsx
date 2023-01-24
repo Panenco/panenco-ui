87f4d79c250226760028818065a1c45a3603ebd9
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2023-01-24 17:40:54 +0100

    feat: paper stories

--- src/components/paper/index.tsx
+++ src/components/paper/index.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { StyledPaper } from './styles';
 
-export const Paper = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ children = 'Content', className, style, ...props }: React.HTMLAttributes<HTMLDivElement>, ref): JSX.Element => {
+export interface PaperProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * Content of the Paper
+   * */
+  children?: React.ReactNode;
+}
+export const Paper = React.forwardRef<HTMLDivElement, PaperProps>(
+  ({ children = 'Content', className, style, ...props }, ref): JSX.Element => {
     return (
       <StyledPaper ref={ref} style={style} className={className} {...props}>
         {children}
