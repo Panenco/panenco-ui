ff99272ea5c2ae0d3a8dce0a81d6ef8259bc8abd
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2023-01-25 13:00:36 +0100

    fix: stamp-colors-and-stories

--- stories/Stamp/Stamp.stories.tsx
+++ stories/Stamp/Stamp.stories.tsx
@@ -9,9 +9,6 @@ export default {
   args: {
     children: 'Stamp content',
   },
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
   decorators: [(Story) => <div style={{ maxWidth: '150px' }}>{Story()}</div>],
 } as ComponentMeta<typeof Stamp>;
 
@@ -20,16 +17,27 @@ const Template: ComponentStory<typeof Stamp> = (args) => <Stamp {...args} />;
 export const Default = Template.bind({});
 
 export const Outlined = Template.bind({});
-
 Outlined.args = {
   variant: 'outlined',
 };
 
 export const Fulfilled = Template.bind({});
-
 Fulfilled.args = {
   variant: 'fulfilled',
+};
+
+export const Color = Template.bind({});
+Color.args = {
   color: colors.primary700,
-  backgroundColor: colors.primary200,
-  borderRadius: 10,
+};
+
+export const BackgroundColor = Template.bind({});
+BackgroundColor.args = {
+  backgroundColor: colors.primary700,
+  variant: 'fulfilled',
+};
+
+export const BorderRadius = Template.bind({});
+BorderRadius.args = {
+  borderRadius: 6,
 };
