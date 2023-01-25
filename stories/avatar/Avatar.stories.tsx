2a37a2e3e433d13cec8e3ed89aa97c2c854f5bc6
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2023-01-25 21:34:09 +0100

    fix: avatar stories

--- stories/avatar/Avatar.stories.tsx
+++ stories/avatar/Avatar.stories.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Avatar as PUIAvatar } from 'components/avatar';
+import { Avatar } from 'components/avatar';
 
 export default {
   title: 'Example/Avatar',
-  component: PUIAvatar,
-
+  component: Avatar,
   args: {
-    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJmD-ID1Gcf4RD0YWdMtgcjjpMqS89ldAE6w&usqp=CAU',
-    imgProps: {
-      alt: 'John Doe',
-    },
     children: 'JD',
-    size: 54,
   },
-} as ComponentMeta<typeof PUIAvatar>;
+  argTypes: {
+    className: { control: 'text' },
+    size: { control: 'number' },
+    src: { control: 'text' },
+    imgProps: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+} as ComponentMeta<typeof Avatar>;
 
-export const Avatar: ComponentStory<typeof PUIAvatar> = (args) => <PUIAvatar {...args} />;
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+export const Default = Template.bind({});
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+  src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJmD-ID1Gcf4RD0YWdMtgcjjpMqS89ldAE6w&usqp=CAU',
+  imgProps: {
+    alt: 'John Doe',
+  },
+};
+
+export const WithText = Template.bind({});
+
+export const CustomSize = Template.bind({});
+CustomSize.args = {
+  size: 64,
+};
